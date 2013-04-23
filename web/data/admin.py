1554debe916a948ef574be91ff7c265a929282c2
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2013-04-23 16:27:37 +0200

    Add remove_from_index admin action on Recipient

--- web/data/admin.py
+++ web/data/admin.py
@@ -1,21 +1,38 @@
-from django.contrib import admin
-from django.contrib.gis import admin as geo_admin
+from django.contrib.gis import admin
 from models import Recipient, GeoRecipient, SchemeType
-# 
+#
 # # class FeedsAdmin(admin.ModelAdmin):
 # #   list_display  = ('title','url', 'is_active', 'category',)
 # #   list_filter = ('category','is_active',)
-# # 
+# #
 # # class FeedItemsAdmin(admin.ModelAdmin):
 # #   list_display  = ('title','url', 'tags','feed',)
-# 
+#
 # from treebeard.admin import TreeAdmin
-# 
+#
 # class LocationAdmin(TreeAdmin):
 #     prepopulated_fields = {"slug": ("name",)}
-# 
-admin.site.register(Recipient)
-admin.site.register(GeoRecipient, geo_admin.GeoModelAdmin)
+#
+
+
+class RecipientAdmin(admin.ModelAdmin):
+    search_fields = ['globalrecipientidx']
+    actions = ['remove_from_index']
+
+    def remove_from_index(self, request, queryset):
+        from haystack import connections as haystack_connections
+
+        for recipient in queryset:
+            for using in haystack_connections.connections_info.keys():
+                backend = haystack_connections[using].get_backend()
+                backend.remove(recipient)
+
+        self.message_user(request, "Removed from search index")
+    remove_from_index.short_description = "Remove from search index"
+
+admin.site.register(Recipient, RecipientAdmin)
+admin.site.register(GeoRecipient, admin.GeoModelAdmin)
 admin.site.register(SchemeType)
+
 # # admin.site.register(FeedItems, FeedItemsAdmin)
 # # admin.site.register(FeedCategories)
