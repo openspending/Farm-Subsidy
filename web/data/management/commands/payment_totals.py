90ac7761d21c779fbfcebb7a315c18bd12c7f42c
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2013-06-07 12:02:39 +0200

    Fix order of opening file, getting stats, writing

--- web/data/management/commands/payment_totals.py
+++ web/data/management/commands/payment_totals.py
@@ -10,10 +10,10 @@ class Command(BaseCommand):
 
     def handle(self, **options):
 
-        file_path = "%s/data/stats/payment_totals.txt" % settings.ROOT_PATH
-        totals_file = open(file_path, 'w')
-
         total_recipients = Recipient.objects.count()
         sum_of_payments = int(Payment.objects.aggregate(total=Sum('amounteuro'))['total'])
 
+        file_path = "%s/data/stats/payment_totals.txt" % settings.ROOT_PATH
+        totals_file = open(file_path, 'w')
         totals_file.write("%s,%s" % (total_recipients, sum_of_payments))
+        totals_file.close()
