6c8b3608e4e414991f945469119042aaf8d05d64
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2013-10-24 21:32:02 +0200

    Refactor transparency score retrieval

--- web/countryinfo/transparency.py
+++ web/countryinfo/transparency.py
@@ -1,14 +1,17 @@
-import csv
-import sys
-
-from data import countryCodes
-from django.conf import settings
 from django.contrib.humanize.templatetags.humanize import ordinal
 from models import TransparencyScore
-  
+
+
 def transparency_score(country):
-    ts = TransparencyScore.objects.get(country=country)
-    return {'rank' : "%s" % (ordinal(ts.rank)),'percent' : ts.score}
+    try:
+        ts = TransparencyScore.objects.get(country=country)
+    except TransparencyScore.DoesNotExist:
+        return None
+    return {
+        'rank': "%s" % (ordinal(ts.rank),),
+        'percent': ts.score
+    }
+
 
 def transparency_list():
-    pass 
+    pass
