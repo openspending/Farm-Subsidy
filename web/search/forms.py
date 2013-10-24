cefa9a2ca74ffca64faa8052ad3b3199262731bb
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2013-10-24 22:53:08 +0200

    Clean up

--- web/search/forms.py
+++ web/search/forms.py
@@ -1,13 +1,16 @@
 from django import forms
-from django import forms
+
 from haystack.forms import SearchForm
 
 
 class SearchForm(SearchForm):
     q = forms.CharField(
-        widget=forms.TextInput(attrs={'id':'q','title':'Enter a company name or place'}),
-        required=True, 
-        label='Search', 
+        widget=forms.TextInput(attrs={
+            'id': 'q',
+            'title': 'Enter a company name or place'
+        }),
+        required=True,
+        label='Search',
         help_text="""e.g. <a href="/search/nestle">Nestle</a> or <a href="/search/windsor">Windsor</a>"""
-        )
+    )
 
