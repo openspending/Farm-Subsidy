312d85000b96492c231b5bd328075da39748e10b
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2013-10-25 17:52:30 +0200

    Add empty string default to name, country search index fields

--- web/data/search_indexes.py
+++ web/data/search_indexes.py
@@ -4,8 +4,8 @@ from .models import Recipient
 
 class RecipientIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
-    name = indexes.CharField(model_attr='name', weight=2)
-    country = indexes.CharField(model_attr='countrypayment', faceted=True)
+    name = indexes.CharField(model_attr='name', default='', weight=2)
+    country = indexes.CharField(model_attr='countrypayment', default='', faceted=True)
     url = indexes.CharField(model_attr='get_absolute_url', indexed=False)
     total = indexes.FloatField(model_attr='total', default=0.0, indexed=False)
 
