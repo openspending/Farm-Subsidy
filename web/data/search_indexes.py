441451ca50e4ba4370e48596054e684624a7bba8
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2013-10-25 12:20:32 +0200

    Add url and total as search index attributes

--- web/data/search_indexes.py
+++ web/data/search_indexes.py
@@ -7,6 +7,8 @@ class RecipientIndex(indexes.SearchIndex, indexes.Indexable):
     name = indexes.CharField(model_attr='name', default="unknown", weight=2)
     country = indexes.CharField(model_attr='countrypayment', default="unknown",
         faceted=True)
+    url = indexes.CharField(model_attr='get_absolute_url')
+    total = indexes.FloatField(model_attr='total', default=0.0)
 
     def get_model(self):
         return Recipient
