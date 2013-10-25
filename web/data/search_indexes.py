65df377b24d9294e4e5395d1b07683af9e6310c3
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2013-10-25 12:28:17 +0200

    Change attribute properties of Recipient index

--- web/data/search_indexes.py
+++ web/data/search_indexes.py
@@ -4,11 +4,10 @@ from .models import Recipient
 
 class RecipientIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
-    name = indexes.CharField(model_attr='name', default="unknown", weight=2)
-    country = indexes.CharField(model_attr='countrypayment', default="unknown",
-        faceted=True)
-    url = indexes.CharField(model_attr='get_absolute_url')
-    total = indexes.FloatField(model_attr='total', default=0.0)
+    name = indexes.CharField(model_attr='name', weight=2)
+    country = indexes.CharField(model_attr='countrypayment', faceted=True)
+    url = indexes.CharField(model_attr='get_absolute_url', indexed=False)
+    total = indexes.FloatField(model_attr='total', default=0.0, indexed=False)
 
     def get_model(self):
         return Recipient
