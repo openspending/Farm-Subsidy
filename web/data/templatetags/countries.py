a7af32bb81394ec9c67f785af2baea3058fbb4a2
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2014-05-10 11:32:08 +0200

    Style right-side country list

--- web/data/templatetags/countries.py
+++ web/data/templatetags/countries.py
@@ -1,19 +1,23 @@
-from django.template import Library, Node
+from django.template import Library
 from data import countryCodes
+
 register = Library()
 
+
 def country_menu():
-  countries = []
-  for country in countryCodes.country_codes():
-    countries.append(countryCodes.country_codes(country))
-  return {'countries' : countries}
-  
+    countries = []
+    for country in countryCodes.country_codes():
+        countries.append(countryCodes.country_codes(country))
+    return {'countries': countries}
+
 register.inclusion_tag('blocks/country_menu.html')(country_menu)
 
-def country_map(country_code):
-    if country_code == "EU":
-        countries = countryCodes.country_codes()[1:]
-        country_code = "|".join(countries)
-    return {"country_code" : country_code}
 
-register.inclusion_tag('blocks/country_map.html')(country_map)    
+def country_map(country_code):
+    if country_code == 'EU':
+        countries = countryCodes.country_codes()[1:]
+        country_code = '|'.join(countries)
+    return {'country_code': country_code}
+
+
+register.inclusion_tag('blocks/country_map.html')(country_map)
